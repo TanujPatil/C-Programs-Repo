0b19029a2a73425a22d5f09cdd713730a9989140
TanujPatil <tanujpatil90@gmail.com>
2022-03-23 09:06:01 +0530

    ADDED:[Tanuj] program for multiplication of given two numbers

---
+++ MULT.C
@@ -0,0 +1,14 @@
+// program for multiplication of given two numbers
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int a,b,c=0;
+	clrscr();
+	printf("\n emter any two numbers=");
+	scanf("%d%d",&a,&b);
+	c=a*b;
+	printf("\n multiplication=",c);
+	getch();
+}
