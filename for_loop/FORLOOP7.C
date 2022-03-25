d9fda3985b6cbe2835f1c0a7386d1c21145f688b
TanujPatil <tanujpatil90@gmail.com>
2022-03-25 09:27:28 +0530

    ADDED:[Tanuj] Program to print mathematical table of 5

---
+++ for_loop/FORLOOP7.C
@@ -0,0 +1,13 @@
+// program to print mathematical table of 2
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int i;
+	clrscr();
+	for(i=1;i<=10;i++)
+	{
+		printf("\n %d",i*5);
+	}
+	getch();
+}
